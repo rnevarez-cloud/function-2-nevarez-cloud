630d5915d09bdba56409c0e8da345e6c65456c8d
Ricardo Nevarez Jr <ricardo@nevarez.cloud>
2025-02-10 12:02:24 -0500

    Created Azure Function to query game scores for certain date

---
+++ function_app.py
@@ -0,0 +1,52 @@
+import azure.functions as func
+import logging
+import datetime
+import json
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+
+@app.function_name(name="get_scores")
+@app.route(route="scores")
+@app.cosmos_db_input(arg_name="inputDocument", 
+                     database_name="game_scores", 
+                     container_name="scores_container",
+                     connection="CosmosDbConnectionSetting")
+@app.cosmos_db_output(arg_name="outputDocument", 
+                     database_name="game_scores", 
+                     container_name="scores_container",
+                     create_if_not_exists=True,
+                     connection="CosmosDbConnectionSetting")
+
+def get_scores(inputDocument: func.DocumentList,
+                outputDocument: func.Out[func.Document],
+                req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    scores = []
+    for d in inputDocument:
+        scores.append(d.to_dict())
+
+    date = req.params.get('date')
+    if not date:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            date = req_body.get('date')
+
+    if not date:
+        date = str(datetime.date.today())
+
+    date_scores = list(filter(lambda x: x['date'] == date, scores))
+    
+    if not date_scores:
+        return func.HttpResponse(
+            "No scores found for the given date.",
+            status_code=200
+    )
+
+    return func.HttpResponse(
+            f"{date_scores}",
+            status_code=200
+    )
