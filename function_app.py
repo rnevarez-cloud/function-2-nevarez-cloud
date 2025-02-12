6fc8dfcbf04aebf083213ec5a4818a78d9659257
Ricardo Nevarez Jr <ricardo@nevarez.cloud>
2025-02-12 15:20:50 -0500

    Hard coding today's date for query. At some point in the future I'll try to add functionality to query previous dates.

--- function_app.py
+++ function_app.py
@@ -3,6 +3,8 @@ import logging
 import datetime
 import json
 
+date = str(datetime.date.today())
+
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.function_name(name="get_scores")
@@ -10,37 +12,24 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 @app.cosmos_db_input(arg_name="inputDocument", 
                      database_name="game_scores", 
                      container_name="scores_container",
+                     sql_query=f"SELECT * FROM c where c.date = '{date}'",
                      connection="CosmosDbConnectionSetting")
 
 def get_scores(inputDocument: func.DocumentList,
                 req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-
+    logging.info(date)
     scores = []
     for d in inputDocument:
         scores.append(d.to_dict())
-
-    date = req.params.get('date')
-    if not date:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            date = req_body.get('date')
-
-    if not date:
-        date = str(datetime.date.today())
-
-    date_scores = list(filter(lambda x: x['date'] == date, scores))
-    
-    if not date_scores:
+   
+    if not scores:
         return func.HttpResponse(
             "No scores found for the given date.",
             status_code=200
     )
 
     return func.HttpResponse(
-            json.dumps(date_scores),
+            json.dumps(scores),
             status_code=200
     )
